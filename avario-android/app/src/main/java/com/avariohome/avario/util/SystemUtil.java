e7e8c6cbac56fb923f516d11105301b300b8209b
John Philip Agustino <wwwdotphilip@gmail.com>
2017-10-24 21:10:26 +0800

    [30] change pendingintentid

--- avario-android/app/src/main/java/com/avariohome/avario/util/SystemUtil.java
+++ avario-android/app/src/main/java/com/avariohome/avario/util/SystemUtil.java
@@ -12,7 +12,7 @@ public class SystemUtil {
 
     public static void rebootApp(Context context){
         Intent mStartActivity = new Intent(context, BootActivity.class);
-        int mPendingIntentId = 123456;
+        int mPendingIntentId = 21443;
         PendingIntent mPendingIntent = PendingIntent.getActivity(context, mPendingIntentId, mStartActivity,
                 PendingIntent.FLAG_CANCEL_CURRENT);
         AlarmManager mgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
